5e1c4df3c77eafcebe293037333c05ad6c6d535e
Dustin Jensen <dustyj1984@gmail.com>
2022-11-21 18:03:30 -0800

    First Push

---
+++ posts/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.urls import reverse
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=128)
+    subtitle = models.CharField(max_length=128)
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse('detail', args=[str(self.id)])
+
+
+
+    
+
+# Create your models here.
---
+++ posts/views.py
@@ -0,0 +1,19 @@
+from django.views.generic import ListView, DetailView  
+from django.views.generic.edit import CreateView 
+from .models import Post
+
+# Create your views here.
+class PostListView(ListView):
+    template_name = "posts/list.html"
+    model = Post
+
+class PostDetailView(DetailView):
+    template_name = "posts/detail.html"
+    model = Post
+
+class PostCreateView(CreateView):
+    template_name = "posts/create.html"
+    model = Post
+    fields = ['title', 'subtitle', 'body']
+
+    
